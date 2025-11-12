5b9cc2b0fff32188bd0c5cc7e441e016d1b0ca6b
Nalani Fierro <nalanimfierro@lewisu.edu>
2025-11-12 10:23:52 -0600

    update file

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+import subprocess
+print("Starting add-commit-push")
+print("git status")
+subprocess.run(["git","status"])
+
+print ("git add -A")
+subprocess.run(["git","add","-A"])
+
+print ("git commit -m")
+subprocess.run(["git","commit","-m","\"update files\""])
+
+print("git push")
+subprocess.run(["git","push"])
+
+
